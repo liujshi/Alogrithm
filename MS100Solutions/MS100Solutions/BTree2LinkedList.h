891a28e4d02ef24d237e628f5947fba481a46d9d
刘俊仕 <liujshi@foxmail.com>
2016-03-27 18:27:37 +0800

    add test

---
+++ MS100Solutions/MS100Solutions/BTree2LinkedList.h
@@ -0,0 +1,58 @@
+// MS100Solutions.cpp : 定义控制台应用程序的入口点。
+//
+
+#pragma once
+#include "Common.h"
+namespace Ljs{
+	class TreeNode
+	{
+	public:
+		int val;
+		TreeNode *left;
+		TreeNode *right;
+		TreeNode(int v = -1) :val(v), left(nullptr), right(nullptr) {}
+
+		void VisitTree(TreeNode *root)
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+			VisitTree(root->left);
+			printf("%d\n", root->val);
+			VisitTree(root->right);
+		}
+
+		void helper(TreeNode *& head, TreeNode *& tail, TreeNode *root) {
+			TreeNode *leftTail, *rightHead;
+			if (root == nullptr)
+			{
+				head = nullptr;
+				tail = nullptr;
+				return;
+			}
+			helper(head, leftTail, root->left);
+			helper(rightHead, tail, root->right);
+			if (leftTail != nullptr)
+			{
+				leftTail->right = root;
+				root->left = leftTail;
+			}
+			else
+				head = root;
+			if (rightHead != nullptr)
+			{
+				rightHead->left = root;
+				root->right = rightHead;
+			}
+			else
+				tail = root;
+		}
+
+		TreeNode * treeToLinkedList(TreeNode * root) {
+			TreeNode * head, *tail;
+			helper(head, tail, root);
+			return head;
+		}
+	};
+}
